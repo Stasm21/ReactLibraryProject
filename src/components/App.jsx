8861f86b9786b1b88ef6af88dbcdc6005cd7ae34
Stas <stasm21@gmail.com>
2021-09-07 20:59:25 -0700

    created previous book and next book button, they are fully functional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,27 +11,66 @@ class App extends Component {
             { title: "The First and Last Freedom", author: "Jiddu Krishnamurit" }
         ];
         this.state = {
-            bookNumber: 1
+            bookNumber: 0
         };
+    }
+
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length) {
+            tempBookNumber = 0;
+        }
+
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length) {
+            tempBookNumber = 0;
+        }
+
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0) {
+            tempBookNumber = this.books.length - 1
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
     }
 
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
-                <div className="row"></div>
-                <div className="col-md-4">
-                    {/*Button here to move to the previous book viewed*/}
-                </div>
-                <div className="col-md-4">
-                    {/*Display book with cover here*/}
-                    <h1>{this.books[this.state.bookNumber].title}</h1>
-                    <h4>{this.books[this.state.bookNumber].author}</h4>
-                </div>
-                <div className="col-md-4">
-                    {/*Button here to move to the next book viewed*/}
+                <div className="row">
+                    <div className="col-md-4">
+                        {/*Button here to move to the previous book viewed*/}
+                        <button onClick={this.goToPreviousBook}>Previous Book</button>
+                    </div>
+                    <div className="col-md-4">
+                        {/*Display book with cover here*/}
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className="col-md-4">
+                        {/*Button here to move to the next book viewed*/}
+                        <button onClick={this.goToNextBook}>Next Book</button>
+                    </div>
                 </div>
             </div>
+
         )
     }
 }
